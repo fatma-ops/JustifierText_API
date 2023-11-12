2eb716308d457e3221429edd2d99415e9fdf3a86
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-11-12 21:36:39 +0100

    vercel

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,7 +3,6 @@ import {Pool} from 'pg' ;
 import { DATABASE_URL } from '../env';
 
 const DataBase = new Pool({
-  connectionString: DATABASE_URL ,
-});
+  connectionString: DATABASE_URL + "?sslmode=require",}); 
 
 export { DataBase };
