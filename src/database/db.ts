0af8f46149cc1b7a6d9c7c95dd54c47f422a60d8
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-11-13 15:37:57 +0100

    changes

--- src/database/db.ts
+++ src/database/db.ts
@@ -3,6 +3,6 @@ import {Pool} from 'pg' ;
 import { DATABASE_URL } from '../env';
 
 const DataBase = new Pool({
-  connectionString: DATABASE_URL + "?sslmode=require",}); 
+  connectionString: DATABASE_URL}); 
 
 export { DataBase };
