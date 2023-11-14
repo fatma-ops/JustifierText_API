3a5d8c03eb6d7e9cf8467970cd329164bd4ae6d7
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-11-14 13:10:06 +0100

    date d'expiration du token

--- src/routes/userToken.ts
+++ src/routes/userToken.ts
@@ -1,35 +1,50 @@
+// Importation du module Router depuis 'express'
 import { Router } from 'express';
+
+// Importation de l'instance de Pool depuis le fichier '../database/db'
 import { DataBase } from '../database/db';
+
+// Importation de la fonction createToken depuis le fichier '../util/createToken'
 import { createToken } from '../util/createToken';
 
+// Expression régulière pour la validation de l'adresse e-mail
 const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
 
+// Création d'une instance de Router pour gérer les routes
 const tokenRouter = Router();
 
+// Route POST '/token' pour la création et la récupération de jetons d'authentification
 tokenRouter.post('/token', async (req, res) => {
+  // Récupération de l'adresse e-mail depuis le corps de la requête
   const email = req.body.email;
   
+  // Vérification si l'adresse e-mail est fournie
   if (!email) {
-    return res.status(400).json({ error: 'email address is required' });
+    return res.status(400).json({ erreur: 'L\'adresse e-mail est requise' });
   }
 
   // Validation de l'e-mail en utilisant l'expression régulière
-  if (!email || !emailRegex.test(email)) {
-    return res.status(400).json({ error: 'invalid email address' });
+  if (!emailRegex.test(email)) {
+    return res.status(400).json({ erreur: 'Adresse e-mail invalide' });
   }
 
+  // Création d'un jeton d'authentification en utilisant la fonction createToken
   const token = createToken(email);
 
   try {
+    // Insertion de l'adresse e-mail dans la base de données, en évitant les conflits
     await DataBase.query('INSERT INTO users(email) VALUES ($1) ON CONFLICT DO NOTHING', [email]);
   } catch (error) {
-    console.error('Error:', error);
-    return res.status(500).json({ error: 'internal error' });
-
+    // Gestion des erreurs lors de l'accès à la base de données
+    console.error('Erreur :', error);
+    return res.status(500).json({ erreur: 'Erreur interne' });
   }
 
+  // Réponse avec le jeton d'authentification
   return res.status(200).json(token);
 });
 
+// Exportation du router pour permettre son utilisation dans d'autres fichiers
 export { tokenRouter };
+
 
--- src/util/createToken.ts
+++ src/util/createToken.ts
@@ -1,9 +1,17 @@
 
+// Importation du module jwt depuis 'jsonwebtoken'
 import jwt from 'jsonwebtoken';
-import { TOKEN_SECRET } from '../env';
 
+// Importation de la clé secrète du token et la durée de validité du token depuis le fichier '../env'
+import { TOKEN_SECRET , TOKEN_EXPIRE} from '../env';
+
+// Fonction pour créer un token d'authentification avec une expiration de 24 heures
 const createToken = (email: string): string => {
-  return jwt.sign(email, TOKEN_SECRET as string );
+  // Signature du token en utilisant l'adresse e-mail et la clé secrète
+  // L'option expiresIn définit la durée de validité du token (ici, 24 heures)
+  return jwt.sign({ email }, TOKEN_SECRET as string, { expiresIn: TOKEN_EXPIRE });
 };
 
+// Exportation de la fonction pour permettre son utilisation dans d'autres fichiers
 export { createToken };
+
