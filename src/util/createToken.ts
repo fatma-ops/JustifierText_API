992d85d91ca6c2917cc6b4d601bb0103984d8c1d
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-11-14 13:46:18 +0100

    remove

--- src/util/createToken.ts
+++ src/util/createToken.ts
@@ -1,17 +1,9 @@
 
-// Importation du module jwt depuis 'jsonwebtoken'
 import jwt from 'jsonwebtoken';
+import { TOKEN_SECRET } from '../env';
 
-// Importation de la clé secrète du token et la durée de validité du token depuis le fichier '../env'
-import { TOKEN_SECRET} from '../env';
-
-// Fonction pour créer un token d'authentification avec une expiration de 24 heures
 const createToken = (email: string): string => {
-  // Signature du token en utilisant l'adresse e-mail et la clé secrète
-  // L'option expiresIn définit la durée de validité du token (ici, 24 heures)
-  return jwt.sign({ email }, TOKEN_SECRET as string);
+  return jwt.sign(email, TOKEN_SECRET as string );
 };
 
-// Exportation de la fonction pour permettre son utilisation dans d'autres fichiers
 export { createToken };
-
