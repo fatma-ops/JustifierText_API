fd099f74564bdb9312647c4eb56bf817d000bcc6
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-11-14 22:00:32 +0100

    authentification token

--- src/middlewares/authmiddlewares.ts
+++ src/middlewares/authmiddlewares.ts
@@ -1,32 +1,25 @@
-// Importation du module jwt pour la gestion des tokens JWT
 import jwt from 'jsonwebtoken';
-// Importation des types Request, Response, NextFunction depuis 'express'
 import { Request, Response, NextFunction } from 'express';
-// Importation de la clé secrète pour la vérification des tokens JWT depuis le fichier '../env'
+
 import { TOKEN_SECRET } from '../env';
 
-// Middleware pour l'authentification basée sur les tokens JWT
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 const authenticationToken = (req: Request, res: Response, next: NextFunction) => {
-
-  // Récupération de l'en-tête 'Authorization' de la requête
   const authHeader = req.headers['authorization'];
-  // Extraction du token du header s'il existe
   const token = authHeader && authHeader.split(' ')[1];
-  // Vérification de la présence d'un token dans la requête
-  if (!token) return res.status(403).json({ error: 'Token non fourni' });
+
+  if (!token) return res.status(403).json({ error: 'No token provided.' });
 
   try {
-  // Vérification et décodage du token à l'aide de la clé secrète
     const decoded = jwt.verify(token, TOKEN_SECRET as string);
-    // Assignation des informations utilisateur décodées à l'objet 'user' dans la requête
-    // @ts-ignore  (permet d'ignorer l'erreur TypeScript car le type de 'user' a été ajouté dynamiquement)
+
+    // @ts-ignore
     req.user = decoded;
 
     next();
-  // Gestion des erreurs en cas de token invalide
   } catch (err) {
     console.log(err);
-    return res.status(403).json({ error: 'La token est invalide ' });
+    return res.status(403).json({ error: 'Invalid token' });
   }
 };
 
