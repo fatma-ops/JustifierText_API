644c596b1589653a9c8375f4d83ace5d06c7085f
fatma-ops <73761826+fatma-ops@users.noreply.github.com>
2023-11-14 22:14:52 +0100

    comment (create token)

--- src/util/createToken.ts
+++ src/util/createToken.ts
@@ -1,10 +1,12 @@
-
-
+// Importation du module jwt depuis 'jsonwebtoken'
 import jwt from 'jsonwebtoken';
+// Importation de la clé secrète du token depuis le fichier '../env'
 import { TOKEN_SECRET } from '../env';
 
+// Fonction pour créer un token d'authentification avec une expiration de 24 heures
 const createToken = (email: string): string => {
+  // Signature du token en utilisant l'adresse e-mail et la clé secrète
   return jwt.sign(email, TOKEN_SECRET as string );
 };
-
+// Exportation de la fonction pour permettre son utilisation dans d'autres fichiers
 export { createToken };
